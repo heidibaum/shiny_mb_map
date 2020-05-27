3b51c3f304370967d570e4d346f01a532e954373
RodrigoDalBen <dalbenwork@gmail.com>
2020-05-27 17:56:07 -0300

    edit groups & data.

--- chapters_source.R
+++ chapters_source.R
@@ -1,6 +1,47 @@
+# Rodrigo Dal Ben
+# 27/05/2020
+# adapted from: https://github.com/rladies/rshinylady
+
+# load library
 library(rvest)
 library(tidyverse)
-library(meetupr)
+library(here)
+library(googlesheets4) # read database from drive
+library(countrycode) # continents from countries
+
+library(meetupr) # data... might not be necessary
+
+# load database 
+members <- googlesheets4::read_sheet("1Q9hQZ-Nop6Vqq3Ezre8waVU_qDsGhnJLsWYPh5nAih8")
+
+## RENAME 
+# exclude any punctuations
+# check abbreviations; add more if necessary
+# differentiate "Americas"
+rladies_groups <- 
+  members %>% 
+  dplyr::mutate(
+    country = gsub("[[:digit:]]+", "", country),
+    country = gsub("[[:punct:]]+", "", country),
+    country = case_when(
+      country == "US" | country == "USA" ~ "United States",
+      country == "UK" ~ "United Kingdom",
+      TRUE ~ country
+      ),
+    continent = countrycode::countrycode(country, "country.name", "continent"),
+    continent = case_when( 
+      country %in% c("United States", "Canada") ~ "North America",
+      continent == "Americas" ~ "Latin America",
+      TRUE ~ continent
+      )
+    ) #%>% 
+  #dplyr::select(-full_address) # consider deleting some columns, like address and institution, lab, 
+                                # keep only institution_lab
+
+## get continent next to country DO THIS WITH SELECT (PRECIOUS STEP)
+rladies_groups <- rladies_groups[, c(1:8, 12, 9:11)] 
+
+##### DELETE IN THE FUTURE
 
 # meetup groups ----------------------------------------------------------
 api_key <- Sys.getenv("meetup_key")
@@ -13,24 +54,33 @@ rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies",
                                           x = all_rladies_groups$name,
                                           ignore.case = TRUE), ]
 
-# Add extra columns to set up popup content in the app
-rladies_groups$fullurl <- paste0("https://www.meetup.com/", rladies_groups$urlname, "/")
-rladies_groups$url <- paste0("<a href='", rladies_groups$fullurl, "'>", rladies_groups$name, "</a>")
+##### 
 
-# Each country/continent
-# We need to exclude remote 
-# The R-Ladies chapter is Remote, ie, they don't have a specific location since
-# everything the group does is online. 
-# But when you create a group on meetup.com, you can't leave the location blank, 
-# you need to specify a city. 
-# Therefore, for the Remote chapter, we used San Francisco as their location even 
-# though there are not "located" in San Francisco.
-groups_usa <- rladies_groups %>% 
-  filter(country == "US", name != "R-Ladies Remote")
-created_usa <- groups_usa %>% 
-  mutate(dt_created = substr(created, 1, 10)) %>% 
-  arrange(desc(dt_created)) %>% 
-  select("city", "state", "country", "dt_created", "members")
+## RENAME VARIABLES ; REPLACE URLS
+# Add extra columns to set up popup content in the app
+rladies_groups$fullurl <- paste0("https://rodrigodalben.github.io/", rladies_groups$studies, "/")
+rladies_groups$url <- paste0("<a href='", rladies_groups$fullurl, "'>", rladies_groups$studies, "</a>")
+
+
+## Each continent - KEEP US AND CANADA SEPARATE FOR NOW, FIX LATER - TOGETHER UNDER "NORTH AMERICA"
+# arranged by studies Z-A (most to less recent), then by institutions name ?? 
+created_usa <- 
+  rladies_groups %>% 
+  filter(country == "United States") %>% 
+  arrange(desc(studies), institution)
+
+
+##### 
+##Not doing this now
+  #groups_usa %>% 
+  #mutate(dt_created = substr(created, 1, 10)) %>% 
+  #arrange(desc(dt_created)) %>% 
+  #select("city", "state", "country", "dt_created", "members")
+
+#####
+
+## If we do not have "country" as an input on member sign-up form (which I think we should have)
+## we can get countries from Timezones as described below:
 
 # For subsetting, the follwing countries, I'm using the time zone as a proxy
 # of where the chapter is located
@@ -42,62 +92,84 @@ created_usa <- groups_usa %>%
 
 
 # Canada
-canada <- sort(unique(rladies_groups[grep("Canada", rladies_groups$timezone),]$country))
-groups_canada <- rladies_groups %>% 
-  filter(country %in% canada)
-created_canada <- groups_canada %>% 
-  mutate(dt_created = substr(created, 1, 10)) %>% 
-  arrange(desc(dt_created)) %>% 
-  select("city", "state", "country", "dt_created", "members")
+
+#canada <- sort(unique(rladies_groups[grep("Canada", rladies_groups$timezone),]$country))
+
+created_canada <- rladies_groups %>% 
+  filter(country == "Canada") %>% 
+  arrange(desc(studies), institution) 
+
+#created_canada <- groups_canada %>% 
+#  mutate(dt_created = substr(created, 1, 10)) %>% 
+#  arrange(desc(dt_created)) %>% 
+#  select("city", "state", "country", "dt_created", "members")
 
 
-# Latin America (AR, BR, CL, CO, "CR" "EC" "MX" "PE" "UY" )  
-latam <- sort(unique(rladies_groups[grep("America", rladies_groups$timezone),]$country))
-groups_latam <- rladies_groups %>% 
-  filter(country %in% latam)
-created_latam <- groups_latam %>% 
-  mutate(dt_created = substr(created, 1, 10)) %>% 
-  arrange(desc(dt_created)) %>% 
-  select("city", "state", "country", "dt_created", "members")
+# Latin America (all Americas countries except US and Canada)
+
+#latam <- sort(unique(rladies_groups[grep("America", rladies_groups$timezone),]$country))
+
+created_latam <- rladies_groups %>% 
+  filter(continent == "Latin America") %>% 
+  arrange(desc(studies), institution) 
+
+#created_latam <- groups_latam %>% 
+#  mutate(dt_created = substr(created, 1, 10)) %>% 
+#  arrange(desc(dt_created)) %>% 
+#  select("city", "state", "country", "dt_created", "members")
 
 # Europe
-europe <- sort(unique(rladies_groups[grep("Europe", rladies_groups$timezone),]$country))
-groups_europe <- rladies_groups %>% 
-  filter(country %in% europe)
+
+#europe <- sort(unique(rladies_groups[grep("Europe", rladies_groups$timezone),]$country))
+created_europe <- rladies_groups %>% 
+  filter(continent == "Europe") %>% 
+  arrange(desc(studies), institution) 
 # Add Tbilisi to the Europe group
 # for some reason Tbilisi uses the timezone `Asia/Tbilisi` therefore
 # it got categorized as Asia. However, Tbilisi is located in Europe
-groups_europe <- rbind(groups_europe,
-                       rladies_groups %>% filter(city == "Tbilisi"))
-created_europe <- groups_europe %>% 
-  mutate(dt_created = substr(created, 1, 10)) %>% 
-  arrange(desc(dt_created)) %>% 
-  select("city", "state", "country", "dt_created", "members")
+#groups_europe <- rbind(groups_europe,
+#                       rladies_groups %>% filter(city == "Tbilisi"))
+
+#created_europe <- groups_europe %>% 
+#  mutate(dt_created = substr(created, 1, 10)) %>% 
+#  arrange(desc(dt_created)) %>% 
+#  select("city", "state", "country", "dt_created", "members")
 
 # Africa
-africa <- sort(unique(rladies_groups[grep("Africa", rladies_groups$timezone),]$country))
-groups_africa <- rladies_groups %>% 
-  filter(country %in% africa)
-created_africa <- groups_africa %>% 
-  mutate(dt_created = substr(created, 1, 10)) %>% 
-  arrange(desc(dt_created)) %>% 
-  select("city", "state", "country", "dt_created", "members")
+
+#africa <- sort(unique(rladies_groups[grep("Africa", rladies_groups$timezone),]$country))
+created_africa <- rladies_groups %>% 
+  filter(continent == "Africa") %>% 
+  arrange(desc(studies), institution) 
+
+#created_africa <- groups_africa %>% 
+#  mutate(dt_created = substr(created, 1, 10)) %>% 
+#  arrange(desc(dt_created)) %>% 
+#  select("city", "state", "country", "dt_created", "members")
 
 # Asia
-asia <- sort(unique(rladies_groups[grep("Asia", rladies_groups$timezone),]$country))
-groups_asia <- rladies_groups %>% 
-  filter(country %in% asia) %>% 
-  filter(city != "Tbilisi") # Remove Tbilisi from the Asia group
-created_asia <- groups_asia %>% 
-  mutate(dt_created = substr(created, 1, 10)) %>% 
-  arrange(desc(dt_created)) %>% 
-  select("city", "state", "country", "dt_created", "members")
 
+#asia <- sort(unique(rladies_groups[grep("Asia", rladies_groups$timezone),]$country))
+created_asia <- rladies_groups %>% 
+  filter(continent == "Asia") %>% 
+  arrange(desc(studies), institution) 
+
+# %>% 
+#  filter(city != "Tbilisi") # Remove Tbilisi from the Asia group
+#created_asia <- groups_asia %>% 
+#  mutate(dt_created = substr(created, 1, 10)) %>% 
+#  arrange(desc(dt_created)) %>% 
+#  select("city", "state", "country", "dt_created", "members")
+
+## RENAME TO OCEANIA! 
 #  Australia/Oceania
-australia <- sort(unique(rladies_groups[grep("Australia|Pacific/Auckland", rladies_groups$timezone),]$country))
+
+#australia <- sort(unique(rladies_groups[grep("Australia|Pacific/Auckland", rladies_groups$timezone),]$country))
 groups_australia <- rladies_groups %>% 
-  filter(country %in% australia)
-created_australia <- groups_australia %>% 
-  mutate(dt_created = substr(created, 1, 10)) %>% 
-  arrange(desc(dt_created)) %>% 
-  select("city", "state", "country", "dt_created", "members")
+  filter(continent == "Oceania") %>% 
+  arrange(desc(studies), institution) 
+
+#created_australia <- groups_australia %>% 
+#  mutate(dt_created = substr(created, 1, 10)) %>% 
+#  arrange(desc(dt_created)) %>% 
+#  select("city", "state", "country", "dt_created", "members")
